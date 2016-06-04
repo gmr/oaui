ce04588954ddb0b1dd8a5f18cde0aa8bb5cda56c
Gavin M. Roy <gavinr@aweber.com>
2016-06-04 00:24:51 -0400

    Designy updates

--- src/js/views/api.js
+++ src/js/views/api.js
@@ -13,18 +13,29 @@ var template = require('./templates/api.hbs');
 
 exports.View = base.View.extend({
 
+  events: {
+    "click .endpoint": "toggleEndpoint"
+  },
+
   template: template,
 
   attributes: {http_methods: ['get', 'post', 'put', 'delete', 'options', 'head']},
 
   initialize: function(options) {
     this.listenTo(this.model, 'change', this.onChange);
-    $(this.el).tab('show');
   },
 
   onChange: function() {
     window.document.title = 'API Documentation: ' + this.model.get('title');
     this.render();
+    $(this.el).find('.panel-body').hide();
+  },
+
+  toggleEndpoint: function(e) {
+    var target = e.currentTarget.getAttribute('data-target');
+    $(target).toggle();
+    $(e.currentTarget).find('.methods').toggle();
+    $(e.currentTarget).find('.endpoint-close').toggle();
   }
 
 });
