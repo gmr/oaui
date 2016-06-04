c8b3bb7c5cf74d26d14ca62d74ecd6a53bde091b
Gavin M. Roy <gavinr@aweber.com>
2016-06-03 23:33:58 -0400

    WIP commit
    More work on building out the API view \o/

--- src/js/views/api.js
+++ src/js/views/api.js
@@ -1,13 +1,25 @@
 'use strict';
 
+var $ = require('jQuery');
 var base = require('./base');
+var bootstrap = require('bootstrap');
+
+var handlebars = require('handlebars/runtime')['default'];
+var helpers = require('handlebars-helpers')({
+  handlebars: handlebars
+});
+
 var template = require('./templates/api.hbs');
 
 exports.View = base.View.extend({
+
   template: template,
+
+  attributes: {http_methods: ['get', 'post', 'put', 'delete', 'options', 'head']},
 
   initialize: function(options) {
     this.listenTo(this.model, 'change', this.onChange);
+    $(this.el).tab('show');
   },
 
   onChange: function() {
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,14 +1,20 @@
 'use strict';
 
+var _ = require('underscore');
 var backbone = require('backbone');
 
 exports.View = backbone.View.extend({
+
   render: function() {
+    var data = {};
+
     if (this.model) {
-      this.$el.html(this.template(this.model.toJSON()));
-    } else {
-      this.$el.html(this.template());
+      _.extend(data, this.model.toJSON());
     }
+    if (this.attributes) {
+      _.extend(data, {attributes: this.attributes});
+    }
+    this.$el.html(this.template(data));
   },
 
   template: function() {
